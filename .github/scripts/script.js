947553c801b5d6840e7867ea32f486d735659d8b
Brandon Ly <brandonly@lostcoding.com>
2023-08-31 14:25:11 -0500

    Update script.js

--- .github/scripts/script.js
+++ .github/scripts/script.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-module.exports = ({github, context}) => {
+module.exports = () => {
     const outputsFile = fs.readFileSync('outputs.json').toString();
     const outputs = JSON.parse(outputsFile);
     const webhook = Object.values(outputs['auto-builder-stack-enhancedApp-stg-DOP-3977-webhooks'])[0]
